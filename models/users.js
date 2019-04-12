aef1cf8737ce0cb227e036fbd53cb34d5134a470
hackagon <phonghiavan@gmail.com>
2019-04-12 20:20:44 +0700

    connect to DB and create user model

---
+++ models/users.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  fullName: { type: String, required: true },
+  phone: { type: String, required: true },
+  DOB: { type: Date, required: true },
+  registerDate: { type: Date, default: new Date() },
+  numberOfTrips: { type: Number },
+  numberOfKms: { type: Number },
+  avatar: { type: String },
+  isActive: { type: Boolean, default: true }
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = {
+    User, UserSchema
+}
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 /* 3rd packages */
 const express = require('express');
+const mongoose = require('mongoose');
 
 /* project packages */
+
+/* Connect DB */
+mongoose.connect('mongodb://localhost:27017/fs03-xedike', {useNewUrlParser: true})
+    .then(console.log("Connected to DB"))
+    .catch(console.log)
 
 /* initialize server */
 const app = express();
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-    console.log('Connected to DB on ' + port)
+    console.log('Connected to server on ' + port)
 })
